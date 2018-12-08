acf7bd3229e621a8450f922eade1d47064c63e19
Larry Mascarenhas <larry.masc@gmail.com>
2018-12-08 00:54:56 +0000

    Class based views. W3-CSS

--- django/core/urls.py
+++ django/core/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 
 # from . import views
-from .views import core_view, home_view, send_email_msg, send_msg
+from .views import core_view, home_view, send_email_msg, sign_up,\
+    view_sent_email, show_all_email
 
 app_name = 'core'
 
 urlpatterns = [
     path('core/', core_view.as_view()),
-    # path('core/send_msg/', send_email_msg.as_view(), name='send_email_msg'),
-    path('core/send_msg/', send_msg, name='send_email_msg'),
-    path('', home_view.as_view()),
+    # Moved to class-view path('core/send_msg/', send_msg, name='send_email_msg'),
+    path('core/send_email_msg/', send_email_msg.as_view(), name='send_email_msg'),
+    # Moved to class-view path('core/signup/', signup, name='signup'),
+    path('core/sign_up/', sign_up.as_view(), name='sign_up'),
+    # path('core/view_sent_email/', view_sent_email),
+    path('core/show_all_email/', show_all_email.as_view(), name="show_all_email"),
+    path('', home_view.as_view(), name='app_home'),
 ]
